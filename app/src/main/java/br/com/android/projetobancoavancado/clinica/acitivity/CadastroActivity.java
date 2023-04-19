9124cb4a2f1ca0c3550a8a0c749537a6dc40df85
Candeias-Vinicius <79974062+Candeias-Vinicius@users.noreply.github.com>
2023-04-19 16:03:00 -0300

    Primeira activity pronta

---
+++ app/src/main/java/br/com/android/projetobancoavancado/clinica/acitivity/CadastroActivity.java
@@ -0,0 +1,16 @@
+package br.com.android.projetobancoavancado.clinica.acitivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.android.projetobancoavancado.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
